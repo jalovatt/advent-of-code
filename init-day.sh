bcb93965ed82e72520dab707e0938e320e0a4981
Adam Lovatt <jalovatt@hotmail.com>
2021-12-01 07:34:03 -0700

    Add Mac support and year detection to day script

--- init-day.sh
+++ init-day.sh
@@ -2,16 +2,25 @@
 
 PADDED_DAY=$(printf %02d $1)
 UNPADDED_DAY=$(printf %01d $1)
+YEAR=$(date +%Y)
 
 DIR=./src/days/$PADDED_DAY
 
-echo $DIR
+echo "Creating $DIR"
 
 [ -d "$DIR" ] && echo "Directory $DIR already exists" && exit
 
-cp ./src/days/__template $DIR -r
+cp -R ./src/days/__template $DIR
 
-xdg-open https://adventofcode.com/2020/day/$UNPADDED_DAY
-xdg-open https://adventofcode.com/2020/day/$UNPADDED_DAY/input
+# Mac
+if [[ $(uname) == 'Darwin' ]]; then
+  CMD=open
+# Linux
+else
+  CMD=xdg-open
+fi
+
+$("$CMD" "https://adventofcode.com/$YEAR/day/$UNPADDED_DAY")
+$("$CMD" "https://adventofcode.com/$YEAR/day/$UNPADDED_DAY/input")
 
 yarn test days/$PADDED_DAY --watch
