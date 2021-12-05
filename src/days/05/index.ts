12103040421ee510274f7c23d132858a904c2d66
Adam Lovatt <jalovatt@hotmail.com>
2021-12-05 09:06:39 -0700

    Solve day 5

---
+++ src/days/05/index.ts
@@ -0,0 +1,52 @@
+import { split } from '../../utilities/processing';
+
+const findOverlaps = (input: string, includeDiagonals = false): number => {
+  const segments = split(input).map((line) => {
+    const [, ...coords] = line.match(/(\d+),(\d+) -> (\d+),(\d+)/) as string[];
+
+    return coords.map((n) => parseInt(n, 10));
+  });
+
+  let overlaps = 0;
+  const field: { [key: string]: number } = {};
+
+  const updateField = (x: number, y: number) => {
+    const key = `${x},${y}`;
+    field[key] = (field[key] || 0) + 1;
+    if (field[key] === 2) {
+      overlaps += 1;
+    }
+  };
+
+  segments.forEach(([x1, y1, x2, y2]) => {
+    if (x1 === x2) {
+      const sign = Math.sign(y2 - y1);
+      const stop = y2 + sign;
+      for (let y = y1; y !== stop; y += sign) {
+        updateField(x1, y);
+      }
+    } else if (y1 === y2) {
+      const sign = Math.sign(x2 - x1);
+      const stop = x2 + sign;
+      for (let x = x1; x !== stop; x += sign) {
+        updateField(x, y1);
+      }
+    } else if (includeDiagonals) {
+      const n = Math.abs(x2 - x1);
+      const xSign = Math.sign(x2 - x1);
+      const ySign = Math.sign(y2 - y1);
+
+      for (let i = 0; i <= n; i += 1) {
+        const x = x1 + i * xSign;
+        const y = y1 + i * ySign;
+
+        updateField(x, y);
+      }
+    }
+  });
+
+  return overlaps;
+};
+
+export const a = (input: string) => findOverlaps(input);
+export const b = (input: string) => findOverlaps(input, true);
---
+++ src/days/05/spec.ts
@@ -0,0 +1,61 @@
+import loadText from '../../utilities/loadText';
+import { a, b } from '.';
+
+const title = 'Hydrothermal Venture';
+
+const input = loadText('input.txt');
+
+const example1 = `
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+`;
+
+describe(`Day ${__filename.match(/\/([^/]+)\/spec/)?.[1]} - ${title}`, () => {
+  describe('Part 1', () => {
+    describe('Tests', () => {
+      test.each([
+        [example1, 5],
+      ])('%p => %p', (given, expected) => {
+        expect(a(given)).toEqual(expected);
+      });
+    });
+
+    describe('Solution', () => {
+      const knownSolution = 7644;
+
+      test(`${knownSolution}`, () => {
+        const solution = a(input);
+
+        expect(solution).toEqual(knownSolution);
+      });
+    });
+  });
+
+  describe('Part 2', () => {
+    describe('Tests', () => {
+      test.each([
+        [example1, 12],
+      ])('%p => %p', (given, expected) => {
+        expect(b(given)).toEqual(expected);
+      });
+    });
+
+    describe('Solution', () => {
+      const knownSolution = 18627;
+
+      test(`${knownSolution}`, () => {
+        const solution = b(input);
+
+        expect(solution).toEqual(knownSolution);
+      });
+    });
+  });
+});
