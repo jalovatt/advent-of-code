35d926df1bdb70f801935eabb2598bd554d5d08b
Adam Lovatt <jalovatt@hotmail.com>
2021-12-20 00:19:16 -0700

    Solve day 20

---
+++ src/days/20/index.ts
@@ -0,0 +1,93 @@
+import { split } from '../../utilities/processing';
+
+type Binary = 1 | 0;
+type Field = Record<string, Binary>;
+
+const WINDOW = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 0],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
+const keyCache: Record<number, string[]> = {};
+const getKey = (y: number, x: number) => {
+  if (!keyCache[y]?.[x]) {
+    if (!keyCache[y]) {
+      keyCache[y] = [];
+    }
+
+    keyCache[y][x] = `${y},${x}`;
+  }
+
+  return keyCache[y][x];
+};
+
+const getWindowValue = (y: number, x: number, field: Field, infiniteAreLit: boolean): number => {
+  const digits = [];
+  for (let i = 0; i < WINDOW.length; i += 1) {
+    digits.push(field[getKey(y + WINDOW[i][0], x + WINDOW[i][1])] ?? (infiniteAreLit ? 1 : 0));
+  }
+
+  return parseInt(digits.join(''), 2);
+};
+
+const countLit = (field: Field) => Object.values(field).reduce((l, r) => l + r, 0 as number);
+
+const run = (input: string, times = 2): number => {
+  const [algo,, ...rawImage] = split(input);
+
+  let field: Field = {};
+
+  rawImage.forEach((row, y) => row.split('').forEach((v, x) => { field[getKey(y, x)] = (v === '#' ? 1 : 0); }));
+
+  let bounds = {
+    yMin: 0,
+    yMax: rawImage.length - 1,
+    xMin: 0,
+    xMax: rawImage[0].length - 1,
+  };
+
+  const infinitesToggle = (algo[0] === '#' && algo[algo.length - 1] === '.');
+
+  for (let i = 0; i < times; i += 1) {
+    const next: Field = {};
+    const nextBounds = { ...bounds };
+    const infiniteAreLit = i % 2 !== 0 && infinitesToggle;
+
+    for (let y = bounds.yMin - 1; y <= bounds.yMax + 1; y += 1) {
+      for (let x = bounds.xMin - 1; x <= bounds.xMax + 1; x += 1) {
+        const wv = getWindowValue(y, x, field, infiniteAreLit);
+        const v = algo[wv] === '#' ? 1 : 0;
+        next[getKey(y, x)] = v;
+
+        if (v) {
+          if (y < nextBounds.yMin) {
+            nextBounds.yMin = y;
+          } else if (y > nextBounds.yMax) {
+            nextBounds.yMax = y;
+          }
+
+          if (x < nextBounds.xMin) {
+            nextBounds.xMin = x;
+          } else if (x > nextBounds.xMax) {
+            nextBounds.xMax = x;
+          }
+        }
+      }
+    }
+
+    field = next;
+    bounds = nextBounds;
+  }
+
+  return countLit(field);
+};
+
+export const a = (input: string) => run(input, 2);
+export const b = (input: string) => run(input, 50);
---
+++ src/days/20/spec.ts
@@ -0,0 +1,58 @@
+import loadText from '../../utilities/loadText';
+import { a, b } from '.';
+
+const title = 'Trench Map';
+
+const input = loadText('input.txt');
+
+const example1 = `
+..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###
+`;
+
+describe(`Day ${__filename.match(/\/([^/]+)\/spec/)?.[1]} - ${title}`, () => {
+  describe('Part 1', () => {
+    describe('Tests', () => {
+      test.each([
+        [example1, 35],
+      ])('%p => %p', (given, expected) => {
+        expect(a(given)).toEqual(expected);
+      });
+    });
+
+    describe('Solution', () => {
+      const knownSolution = 4928;
+
+      test(`${knownSolution}`, () => {
+        const solution = a(input);
+
+        expect(solution).toEqual(knownSolution);
+      });
+    });
+  });
+
+  describe('Part 2', () => {
+    describe('Tests', () => {
+      test.each([
+        [example1, 3351],
+      ])('%p => %p', (given, expected) => {
+        expect(b(given)).toEqual(expected);
+      });
+    });
+
+    describe('Solution', () => {
+      const knownSolution = 16605;
+
+      test(`${knownSolution}`, () => {
+        const solution = b(input);
+
+        expect(solution).toEqual(knownSolution);
+      });
+    });
+  });
+});
