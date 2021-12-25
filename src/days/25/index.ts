8eab983851700a2fb9e44f0e8c7da6f966868fc0
Adam Lovatt <jalovatt@hotmail.com>
2021-12-25 14:33:37 -0700

    Solve day 25

---
+++ src/days/25/index.ts
@@ -0,0 +1,145 @@
+/* eslint-disable no-bitwise */
+import circuitBreaker from '../../utilities/circuitBreaker';
+import { log } from '../../utilities/logging';
+import { split } from '../../utilities/processing';
+
+const ECuke = '>';
+const SCuke = 'v';
+type Cucumber = typeof ECuke | typeof SCuke;
+type CucumberField = Record<number, Cucumber>;
+
+const encodeCoord = (y: number, x: number): number => (y << 8) + x;
+const decodeCoord = {
+  y: (coord: number): number => coord >> 8,
+  x: (coord: number): number => coord & 0b11111111,
+};
+
+const wrap = (n: number, m: number): number => n % m;
+
+const prepareField = (input: string): [CucumberField, number, number] => {
+  const field: CucumberField = {};
+
+  const lines = split(input);
+  const height = lines.length;
+  const width = lines[0].trim().length;
+
+  for (let y = 0; y < lines.length; y += 1) {
+    const l = lines[y].trim();
+
+    for (let x = 0; x < l.length; x += 1) {
+      const c = l[x];
+
+      // eslint-disable-next-line no-continue
+      if (c === '.') { continue; }
+
+      field[encodeCoord(y, x)] = c as Cucumber;
+    }
+  }
+
+  return [field, height, width];
+};
+
+export class CucumberSim {
+  field: CucumberField;
+  stepsTaken: number;
+
+  height: number;
+  width: number;
+
+  constructor(input: string) {
+    [this.field, this.height, this.width] = prepareField(input);
+    this.stepsTaken = 0;
+  }
+
+  // Returns number of moves
+  step(): number {
+    const next: CucumberField = {};
+    let moved = 0;
+
+    const southToVisit = [];
+
+    for (let y = 0; y < this.height; y += 1) {
+      for (let x = 0; x < this.width; x += 1) {
+        const coord = encodeCoord(y, x);
+        const c = this.field[coord];
+
+        // eslint-disable-next-line no-continue
+        if (!c) { continue; }
+
+        if (c === SCuke) {
+          southToVisit.push(coord);
+        } else {
+          const target = encodeCoord(y, wrap(x + 1, this.width));
+          if (!this.field[target]) {
+            next[target] = ECuke;
+            moved += 1;
+          } else {
+            next[coord] = ECuke;
+          }
+        }
+      }
+    }
+
+    for (let i = 0; i < southToVisit.length; i += 1) {
+      const coord = southToVisit[i];
+
+      const target = encodeCoord(
+        wrap(decodeCoord.y(coord) + 1, this.height),
+        decodeCoord.x(coord),
+      );
+
+      if (!(this.field[target] === SCuke || next[target] === ECuke)) {
+        next[target] = SCuke;
+        moved += 1;
+      } else {
+        next[coord] = SCuke;
+      }
+    }
+
+    this.stepsTaken += 1;
+    this.field = next;
+    return moved;
+  }
+
+  runUntilStopped(): number {
+    let t = 0;
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const moved = this.step();
+      t += 1;
+      if (!moved) { break; }
+
+      // eslint-disable-next-line @typescript-eslint/no-loop-func
+      circuitBreaker(10000, () => log(`t=${t}, ${moved} cucumbers moved`));
+    }
+
+    return t;
+  }
+
+  describe() {
+    const arr: string[] = [];
+
+    for (let y = 0; y < this.height; y += 1) {
+      const row = [];
+
+      for (let x = 0; x < this.width; x += 1) {
+        row.push(this.field[encodeCoord(y, x)] || '.');
+      }
+
+      arr.push(row.join(''));
+    }
+
+    return arr.join('\n');
+  }
+
+  print() {
+    log(this.describe());
+  }
+}
+
+export const a = (input: string) => {
+  const sim = new CucumberSim(input);
+
+  return sim.runUntilStopped();
+};
---
+++ src/days/25/spec.ts
@@ -0,0 +1,132 @@
+import loadText from '../../utilities/loadText';
+import { a, CucumberSim } from '.';
+
+const title = 'Sea Cucumber';
+
+const input = loadText('input.txt');
+
+const example1 = [`
+..........
+.>v....v..
+.......>..
+..........
+`, `
+..........
+.>........
+..v....v>.
+..........
+`,
+];
+
+const example2 = [`
+...>...
+.......
+......>
+v.....>
+......>
+.......
+..vvv..
+`, `
+..vv>..
+.......
+>......
+v.....>
+>......
+.......
+....v..
+`];
+
+const example3 = [`
+v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>
+`, `
+....>.>v.>
+v.v>.>v.v.
+>v>>..>v..
+>>v>v>.>.v
+.>v.v...v.
+v>>.>vvv..
+..v...>>..
+vv...>>vv.
+>.v.v..v.v
+`];
+
+const example4 = [`
+v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>
+`, `
+..>>v>vv..
+..v.>>vv..
+..>>v>>vv.
+..>>>>>vv.
+v......>vv
+v>v....>>v
+vvv.....>>
+>vv......>
+.>v.vv.v..
+`];
+
+describe(`Day ${__filename.match(/\/([^/]+)\/spec/)?.[1]} - ${title}`, () => {
+  describe('Parsing', () => {
+    test.each([
+      ['...>>>>>...'],
+      ['...>>>>.>..'],
+      [example1[0]],
+      [example2[0]],
+      [example3[0]],
+    ])('%p => %p', (given) => {
+      const sim = new CucumberSim(given);
+
+      expect(sim.describe()).toEqual(given.trim());
+    });
+  });
+
+  describe('Step', () => {
+    test.each([
+      ['...>>>>>...', '...>>>>.>..'],
+      ['...>>>>.>..', '...>>>.>.>.'],
+      example1,
+      example2,
+      example3,
+    ])('%p => %p', (given, expected) => {
+      const sim = new CucumberSim(given);
+      sim.step();
+
+      expect(sim.describe()).toEqual(expected.trim());
+    });
+  });
+
+  describe('Part 1', () => {
+    describe('Tests', () => {
+      test.each([
+        [example4[0], 58],
+      ])('%p => %p', (given, expected) => {
+        expect(a(given)).toEqual(expected);
+      });
+    });
+
+    describe('Solution', () => {
+      const knownSolution = 337;
+
+      test(`${knownSolution}`, () => {
+        const solution = a(input);
+
+        expect(solution).toEqual(knownSolution);
+      });
+    });
+  });
+});
