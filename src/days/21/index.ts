3076cf94af243198dfef8ba2cd0d7b9576fb690d
Adam Lovatt <jalovatt@hotmail.com>
2021-12-25 22:14:03 -0700

    Solve day 21 part 2

--- src/days/21/index.ts
+++ src/days/21/index.ts
@@ -1,6 +1,7 @@
+/* eslint-disable no-bitwise */
 import { split } from '../../utilities/processing';
 
-const ring = (n: number, l: number) => ((n - 1) % l) + 1;
+export const ring = (n: number, l: number) => ((n - 1) % l) + 1;
 
 class DeterministicDie {
   rollCount: number;
@@ -45,4 +46,147 @@ export const a = (input: string) => {
   return players[currentPlayer].score * die.rollCount;
 };
 
-export const b = (input: string) => {};
+const ROLL_OUTCOMES = [
+  [3, 1],
+  [4, 3],
+  [5, 6],
+  [6, 7],
+  [7, 6],
+  [8, 3],
+  [9, 1],
+];
+
+export const encodeState = (p1s: number, p2s: number, p1p: number, p2p: number, turn: number) => (
+  (((((((p1s << 5) + p2s) << 4) + p1p) << 4) + p2p) << 1) + turn
+);
+
+export const decodeState = (state: number) => ({
+  turn: state & 1,
+  p2p: (state >>> 1) & 0b1111,
+  p1p: (state >>> 5) & 0b1111,
+  p2s: (state >>> 9) & 0b11111,
+  p1s: (state >>> 14) & 0b11111,
+});
+
+/*
+  Storing game state:
+    P1 score: 0-20  5 bits
+    P2 score: 0-20  5 bits
+    P1 pos: 1-10    4 bits
+    P2 pos: 1-10    4 bits
+    Turn:           1 bit
+                    ------
+                    19 bits
+
+  p1s    p2s    p1p  p2p   t
+  10101  10101  100  1000  0
+     21     21    4     8  0 (p1 turn)
+
+  101011010110010000
+*/
+
+type StateTree = Record<number, [number, number][]>;
+
+const P1_WIN = -1;
+const P2_WIN = -2;
+
+const describeState = (state: number) => {
+  if (state === P1_WIN) { return 'P1 Win'; }
+  if (state === P2_WIN) { return 'P2 Win'; }
+  return decodeState(state);
+};
+
+const prepareStateTree = (initialState: number, winningScore = 21): StateTree => {
+  const stateTree: StateTree = {};
+  const toVisit = [initialState];
+
+  while (toVisit.length) {
+    const cur = toVisit.pop()!;
+    // eslint-disable-next-line no-continue
+    if (stateTree[cur]) { continue; }
+
+    const state = decodeState(cur);
+
+    stateTree[cur] = [];
+
+    for (let i = 0; i < ROLL_OUTCOMES.length; i += 1) {
+      const v = ROLL_OUTCOMES[i][0];
+      const count = ROLL_OUTCOMES[i][1];
+
+      /* eslint-disable prefer-destructuring */
+      let p1p = state.p1p;
+      let p2p = state.p2p;
+      let p1s = state.p1s;
+      let p2s = state.p2s;
+      /* eslint-enable prefer-destructuring */
+
+      let nextState;
+
+      if (!state.turn) {
+        p1p = ring(p1p + v, 10);
+        p1s += p1p;
+
+        if (p1s >= winningScore) {
+          nextState = P1_WIN;
+        }
+      } else {
+        p2p = ring(p2p + v, 10);
+        p2s += p2p;
+
+        if (p2s >= winningScore) {
+          nextState = P2_WIN;
+        }
+      }
+
+      nextState ??= encodeState(p1s, p2s, p1p, p2p, (state.turn + 1) & 1);
+
+      if (!stateTree[nextState] && nextState !== P1_WIN && nextState !== P2_WIN) {
+        toVisit.push(nextState);
+      }
+
+      stateTree[cur].push([nextState, count]);
+    }
+  }
+
+  return stateTree;
+};
+
+// Count of the player that wins in the most paths
+export const b = (input: string, winningScore?: number) => {
+  const [p1, p2] = split(input).map((line) => parseInt(line.match(/(\d+)$/)![1], 10));
+
+  const initialState = (((p1 << 4) + p2) << 1);
+  const stateTree = prepareStateTree(initialState, winningScore);
+
+  const solvedStates: Record<number, [number, number]> = {};
+
+  const solveState = (state: number): [number, number] => {
+    const nextStates = stateTree[state];
+
+    const wins: [number, number] = [0, 0];
+
+    for (let i = 0; i < nextStates.length; i += 1) {
+      const next = nextStates[i][0];
+      const count = nextStates[i][1];
+
+      if (next === P1_WIN) {
+        wins[0] += count;
+      } else if (next === P2_WIN) {
+        wins[1] += count;
+      } else {
+        if (!solvedStates[next]) {
+          solvedStates[next] = solveState(next);
+        }
+
+        wins[0] += solvedStates[next][0] * count;
+        wins[1] += solvedStates[next][1] * count;
+      }
+    }
+
+    return wins;
+  };
+
+  solvedStates[initialState] = solveState(initialState);
+
+  return Math.max(solvedStates[initialState][0], solvedStates[initialState][1]);
+};
--- src/days/21/spec.ts
+++ src/days/21/spec.ts
@@ -1,5 +1,6 @@
+/* eslint-disable object-curly-newline */
 import loadText from '../../utilities/loadText';
-import { a, b } from '.';
+import { a, b, decodeState, encodeState, ring } from '.';
 
 const title = 'Dirac Dice';
 
@@ -10,11 +11,71 @@ Player 1 starting position: 4
 Player 2 starting position: 8
 `;
 
-const example2 = `
-1
-`;
-
 describe(`Day ${__filename.match(/\/([^/]+)\/spec/)?.[1]} - ${title}`, () => {
+  describe('ring', () => {
+    test.each([
+      [[1, 1], 2],
+      [[1, 9], 10],
+      [[1, 10], 1],
+      [[7, 9], 6],
+      [[10, 9], 9],
+      [[4, 3], 7],
+      [[4, 7], 1],
+      [[8, 3], 1],
+      [[8, 5], 3],
+    ])('%p => %p', (given, expected) => {
+      expect(ring(given[0] + given[1], 10)).toEqual(expected);
+    });
+  });
+
+  describe('Encoding state', () => {
+    test('basic encode', () => {
+      expect(encodeState(10, 5, 6, 3, 0)).toEqual(0b0101000101011000110);
+
+      expect(encodeState(1, 0, 1, 4, 1)).toEqual(16425);
+    });
+
+    test('basic decode', () => {
+      expect(decodeState(0b0101000101011000110)).toEqual({
+        p1s: 10,
+        p2s: 5,
+        p1p: 6,
+        p2p: 3,
+        turn: 0,
+      });
+
+      expect(decodeState(16425)).toEqual({ p1p: 1, p2p: 4, p1s: 1, p2s: 0, turn: 1 });
+    });
+
+    test.each([
+      { p1s: 0, p2s: 0, p1p: 4, p2p: 8, turn: 0 },
+      { p1s: 1, p2s: 0, p1p: 1, p2p: 8, turn: 1 },
+      { p1s: 1, p2s: 2, p1p: 1, p2p: 10, turn: 0 },
+      { p1s: 14, p2s: 2, p1p: 4, p2p: 10, turn: 1 },
+      { p1p: 1, p2p: 4, p1s: 1, p2s: 0, turn: 1 },
+    ])('%p => %p', (given) => {
+      const encoded = encodeState(given.p1s, given.p2s, given.p1p, given.p2p, given.turn);
+      expect(decodeState(encoded)).toEqual(given);
+    });
+
+    xtest('all states', () => {
+      for (let p1s = 0; p1s <= 20; p1s += 1) {
+        for (let p2s = 0; p2s <= 20; p2s += 1) {
+          for (let p1p = 1; p1p <= 10; p1p += 1) {
+            for (let p2p = 1; p2p <= 10; p2p += 1) {
+              for (let turn = 0; turn <= 1; turn += 1) {
+                const encoded = encodeState(p1s, p2s, p1p, p2p, turn);
+                expect(decodeState(encoded)).toEqual({
+                  p1s, p2s, p1p, p2p, turn,
+                });
+              }
+            }
+          }
+        }
+      }
+    });
+  });
+
   describe('Part 1', () => {
     describe('Tests', () => {
       test.each([
@@ -35,17 +96,37 @@ describe(`Day ${__filename.match(/\/([^/]+)\/spec/)?.[1]} - ${title}`, () => {
     });
   });
 
-  xdescribe('Part 2', () => {
+  describe('Part 2', () => {
     describe('Tests', () => {
       test.each([
-        [example2, 2],
+        [1, 27], // 0)
+        [2, 183], // 156)
+        [3, 990], // 207)
+        [4, 2930], // 971)
+        [5, 7907], // 2728)
+        [6, 30498], // 7203)
+        // [7, 127019], // 152976)   These three aren't matching up to the list
+        // [8, 655661], // 1048978)  I found but I don't feel like looking into it
+        // [9, 4008007], // 4049420)
+        [10, 18973591], // 12657100)
+        [11, 90197150], // 47304735
+        [12, 454323519], // 217150220
+        [13, 2159295972], // 1251104269
+        [14, 9632852745], // 7543855038
+        [15, 43413388231], // 37334719860
+        [16, 199092281721], // 161946691198
+        [17, 903307715712], // 698632570521
+        [18, 4227532541969], // 3151502992942
+        [19, 20259464849183], // 14795269706204
+        [20, 95627706087732], // 71421811355805
+        [21, 444356092776315], // 341960390180808
       ])('%p => %p', (given, expected) => {
-        expect(b(given)).toEqual(expected);
+        expect(b(example1, given)).toEqual(expected);
       });
     });
 
-    xdescribe('Solution', () => {
-      const knownSolution = null;
+    describe('Solution', () => {
+      const knownSolution = 275067741811212;
 
       test(`${knownSolution}`, () => {
         const solution = b(input);
