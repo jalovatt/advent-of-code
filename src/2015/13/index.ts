8ffd99496108c7ee94e3fe7cc41fac05b6e48829
Adam Lovatt <jalovatt@hotmail.com>
2022-01-09 13:55:23 -0700

    Solve 2015 day 13

---
+++ src/2015/13/index.ts
@@ -0,0 +1,108 @@
+import circuitBreaker from '@lib/circuitBreaker';
+import { inspect } from '@lib/logging';
+import { split } from '@lib/processing';
+
+type Node = { name: string, index: number, edges: Record<string, number> };
+type Nodes = Record<string, Node>;
+
+type State = { at: string, visited: number, happiness: number };
+
+const parseInput = (input: string, addSelf = false): [string[], Nodes, number] => {
+  const nodes: Nodes = {};
+  const names: string[] = [];
+  let nextIndex = 1;
+
+  split(input).forEach((line) => {
+    // Alice would gain 54 happiness units by sitting next to Bob.
+    const [, a, sign, n, b] = line.match(/(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)./)!;
+
+    if (!nodes[a]) {
+      nodes[a] = {
+        name: a,
+        index: nextIndex,
+        edges: {},
+      };
+
+      nextIndex *= 2;
+      names.push(a);
+    }
+    if (!nodes[b]) {
+      nodes[b] = {
+        name: b,
+        index: nextIndex,
+        edges: {},
+      };
+
+      nextIndex *= 2;
+      names.push(b);
+    }
+
+    const v = (sign === 'gain' ? 1 : -1) * parseInt(n, 10);
+
+    if (Number.isNaN(v)) {
+      throw new Error(`Error parsing line: ${inspect({ line, a, sign, n, b })}`);
+    }
+
+    nodes[a].edges[b] = (nodes[a].edges[b] || 0) + v;
+    nodes[b].edges[a] = (nodes[b].edges[a] || 0) + v;
+  });
+
+  if (addSelf) {
+    nodes.Me = { name: 'Me', index: nextIndex, edges: {} };
+    nextIndex *= 2;
+
+    for (let i = 0; i < names.length; i += 1) {
+      nodes.Me.edges[names[i]] = 0;
+      nodes[names[i]].edges.Me = 0;
+    }
+
+    names.push('Me');
+  }
+
+  return [names, nodes, nextIndex - 1];
+};
+
+/*
+  TODO: Improve perf by using indices for everything. Remove names entirely, keep
+  a couple of global records for At and Happiness, and only have to push a number
+  into toCheck for each new state.
+*/
+const solve = (input: string, addSelf = false): number => {
+  const [names, nodes, ALL_VISITED_MASK] = parseInput(input, addSelf);
+
+  const solutions: State[] = [];
+
+  const toCheck: State[] = [{ at: 'Alice', visited: nodes.Alice.index, happiness: 0 }];
+  while (toCheck.length) {
+    circuitBreaker(200000);
+
+    const cur = toCheck.pop()!;
+
+    for (let i = 0; i < names.length; i += 1) {
+      const name = names[i];
+
+      if ((cur.visited & nodes[name].index) === 0) {
+        const next: State = {
+          at: name,
+          visited: cur.visited | nodes[name].index,
+          happiness: cur.happiness + nodes[cur.at].edges[name],
+        };
+
+        if (next.visited === ALL_VISITED_MASK) {
+          solutions.push({ ...next, happiness: next.happiness + nodes[name].edges.Alice });
+        } else {
+          toCheck.push(next);
+        }
+      }
+    }
+  }
+
+  if (!solutions) {
+    throw new Error('Could not solve');
+  }
+
+  return solutions.sort((a, b) => b.happiness - a.happiness)[0].happiness;
+};
+
+export const part1 = solve;
+export const part2 = (input: string): number => solve(input, true);
---
+++ src/2015/13/spec.ts
@@ -0,0 +1,56 @@
+import describeDay from '@lib/describeDay';
+import loadText from '@lib/loadText';
+import { part1, part2 } from '.';
+
+const title = 'Knights of the Dinner Table';
+
+const input = loadText('input.txt');
+
+const example1 = `
+Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.
+`;
+
+describeDay(title, () => {
+  describe('Part 1', () => {
+    describe('Tests', () => {
+      test.each([
+        [example1, 330],
+      ])('%p => %p', (given, expected) => {
+        expect(part1(given)).toEqual(expected);
+      });
+    });
+
+    describe('Solution', () => {
+      const knownSolution = 709;
+
+      test(`${knownSolution}`, () => {
+        const solution = part1(input);
+
+        expect(solution).toEqual(knownSolution);
+      });
+    });
+  });
+
+  describe('Part 2', () => {
+    describe('Solution', () => {
+      const knownSolution = 668;
+
+      test(`${knownSolution}`, () => {
+        const solution = part2(input);
+
+        expect(solution).toEqual(knownSolution);
+      });
+    });
+  });
+});
